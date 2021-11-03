9d6146b9aacf96e66d29ed1513f210b0c6b1f748
Sergey Chivirda <si.chivirda@gmail.com>
2021-11-03 21:04:33 +0300

    feat: Добавил сущность Request. Сделал первую миграцию.

---
+++ src/Entity/Request.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+
+class Request
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createAt;
+}
